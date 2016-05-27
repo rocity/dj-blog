c366efe2300e8c65e8318a5dfc16822c184c05ce
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-05-27 15:31:42 +0800

    created essential models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    frequency = models.IntegerField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    status = models.IntegerField()
+    created = models.DateTimeField('Date Created')
+    published = models.DateTimeField('Date Published')
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    status = models.IntegerField()
+    created = models.DateTimeField('Date Created')
+    email = models.EmailField()
+    url = models.URLField()
