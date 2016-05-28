ee9ac6c002e7b85ca7385ccdbdc426e3d7bfaa8f
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-05-28 10:13:42 +0800

    added basic content to user facing blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'polls'
+app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<post_id>[0-9]+)/$', views.view, name='view'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html', {})
+    latest_blog_posts = Post.objects.order_by('-created')[:5]
+    return render(request, 'blog/index.html', {'posts': latest_blog_posts})
 
-def detail(request):
-    pass
+def view(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blog/view.html', {'post': post})
